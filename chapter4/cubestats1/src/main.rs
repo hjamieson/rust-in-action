a53a681777bcd43b0b047e00e69dea899f0a6fdc
Hugh Jamieson <hjamieson5@gmail.com>
2024-02-12 19:35:57 -0500

    listing 4.1

---
+++ chapter4/cubestats1/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(sat_id: u64) -> StatusMessage {
+    StatusMessage::Ok
+}
+fn main() {
+    let sat_a:u64 = 0;
+    let sat_b:u64 = 1;
+    let sat_c:u64 = 2;
+
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+
+    println!("a:{:?}, b:{:?}, c:{:?}", a_status, b_status, c_status);
+}
